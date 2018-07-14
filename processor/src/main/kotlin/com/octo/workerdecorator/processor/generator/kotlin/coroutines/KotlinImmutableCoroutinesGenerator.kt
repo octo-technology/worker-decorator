5b69aec679e8d6449b78288672fe82747ef75a08
Maxime Bonnet <mbonnet@octo.com>
2018-07-14 14:54:55 +0200

    First implementation of coroutines decorations

--- processor/src/main/kotlin/com/octo/workerdecorator/processor/generator/kotlin/coroutines/KotlinImmutableCoroutinesGenerator.kt
+++ processor/src/main/kotlin/com/octo/workerdecorator/processor/generator/kotlin/coroutines/KotlinImmutableCoroutinesGenerator.kt
@@ -1,11 +1,55 @@
 package com.octo.workerdecorator.processor.generator.kotlin.coroutines
 
+import com.octo.kotlinelements.asKotlinTypeName
 import com.octo.workerdecorator.processor.Generator
 import com.octo.workerdecorator.processor.entity.DecorationDocument
+import com.squareup.kotlinpoet.*
+import kotlin.coroutines.experimental.CoroutineContext
 
 class KotlinImmutableCoroutinesGenerator : Generator {
 
     override fun generate(document: DecorationDocument): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val decoratedType = document.typeMirror.asTypeName()
+
+        val functions = document.methods.map {
+
+            val specParameters = it.parameters.map {
+                var typeName = it.typeMirror.asTypeName()
+                if (document.interfaceIsInKotlin) {
+                    typeName = typeName.asKotlinTypeName()
+                }
+                typeName = if (it.isOptional) {
+                    typeName.asNullable()
+                } else {
+                    typeName.asNonNullable()
+                }
+
+                ParameterSpec.builder(it.name, typeName).build()
+            }
+            val bodyParameters = it.parameters.joinToString(", ") { it.name }
+
+
+            FunSpec.builder(it.name)
+                    .addModifiers(KModifier.OVERRIDE)
+                    .addParameters(specParameters)
+                    .addStatement("kotlinx.coroutines.experimental.launch(coroutineContext) { decorated.${it.name}($bodyParameters) }")
+                    .build()
+        }.asIterable()
+        val decoration = TypeSpec.classBuilder(document.name)
+                .addSuperinterface(decoratedType)
+                .primaryConstructor(
+                        FunSpec.constructorBuilder()
+                                .addParameter("decorated", decoratedType)
+                                .addParameter("coroutineContext", CoroutineContext::class)
+                                .build()
+                )
+                .addProperty(PropertySpec.builder("decorated", decoratedType, KModifier.PRIVATE).initializer("decorated").build())
+                .addProperty(PropertySpec.builder("coroutineContext", CoroutineContext::class, KModifier.PRIVATE).initializer("coroutineContext").build())
+                .addFunctions(functions)
+                .build()
+
+        val source = FileSpec.get(document.`package`, decoration)
+        return source.toString()
     }
+
 }
--- processor/src/test/resources/ExpectedSimpleInterfaceCoroutinesDecoration.kt
+++ processor/src/test/resources/ExpectedSimpleInterfaceCoroutinesDecoration.kt
@@ -1,15 +1,15 @@
 package com.octo.workerdecorator.processor.test.fixture
 
-import kotlinx.coroutines.experimental.launch
-import java.util.*
+import java.util.Date
+import kotlin.String
 import kotlin.coroutines.experimental.CoroutineContext
 
 class KotlinInterfaceDecorated(private val decorated: KotlinInterface, private val coroutineContext: CoroutineContext) : KotlinInterface {
     override fun pam() {
-        launch(coroutineContext) { decorated.pam() }
+        kotlinx.coroutines.experimental.launch(coroutineContext) { decorated.pam() }
     }
 
     override fun jim(arg0: String, arg1: Date?) {
-        launch(coroutineContext) { decorated.jim(arg0, arg1) }
+        kotlinx.coroutines.experimental.launch(coroutineContext) { decorated.jim(arg0, arg1) }
     }
 }
