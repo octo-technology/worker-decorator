db8dc8ddfb1731b11f89fc6d14311ec23da1a033
Maxime Bonnet <mbonnet@octo.com>
2018-08-23 11:29:30 +0200

    Add coroutines integration tests

--- integration-kotlin-coroutines/src/test/kotlin/com/octo/workerdecorator/integration/WorkerDecoratorTest.kt
+++ integration-kotlin-coroutines/src/test/kotlin/com/octo/workerdecorator/integration/WorkerDecoratorTest.kt
@@ -1,6 +1,12 @@
 package com.octo.workerdecorator.integration
 
+import com.octo.workderdecorator.integration.KotlinSimpleInterface
+import com.octo.workderdecorator.integration.KotlinSimpleInterfaceDecorated
+import com.octo.workerdecorator.WorkerDecorator
+import kotlinx.coroutines.experimental.CommonPool
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import java.util.*
 
 /**
  * Very weak tests just to check that the decorations and helpers are generated and that it all compiles
@@ -8,7 +14,22 @@ import org.junit.Test
 class WorkerDecoratorTest {
 
     @Test
-    fun `test stuff`() {
-        // TODO !
+    fun `returns a coroutines decoration with the helper`() {
+        val coroutineContext = CommonPool
+        val decorated = WorkerDecorator.decorate(Implementation(), coroutineContext)
+
+        assertThat(decorated).isInstanceOf(KotlinSimpleInterfaceDecorated::class.java)
+        assertThat(decorated).isInstanceOf(KotlinSimpleInterface::class.java)
     }
+
+    inner class Implementation : KotlinSimpleInterface {
+        override fun a(param: Int) {
+            // stuff
+        }
+
+        override fun b(param: Date) {
+            // stuff
+        }
+    }
+
 }
